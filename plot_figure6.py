b1e9d6b0f038eac87a802aa67489821638f54761
Nicolas Koh <ckoh@princeton.edu>
2020-11-24 22:28:06 -0500

    Scripts for Figure 6

---
+++ plot_figure6.py
@@ -0,0 +1,150 @@
+from parse.filedata import FileData
+
+import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf as pdf
+import pandas as pd
+
+import argparse
+import os
+
+warning_color = '\033[93m'
+end_color = '\033[0m'
+
+def print_warning(s):
+    print(warning_color + 'WARNING: %s' % s + end_color)
+
+def plot_against_time(logs, key, out_file, label=None, log_scale=False):
+    if label is None:
+        label = key
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Time (ms)')
+    ax.set_ylabel('%s' % key)
+    ax.set_title('%s against time' % label)
+
+    names = []
+    for log in logs:
+        contents = log.get_contents()
+        if 'time' in contents and key in contents:
+            names.append(log.get_label())
+            contents.plot(x='time', y=key, ax=ax, legend=True)
+            print('%s: %s[%s] ok' % (log.get_filename(), log.get_label(), key))
+            # print('%s: %s[%s]:' % (log.get_filename(), log.get_label(), key))
+            # print(contents[key])
+        else:
+            print_warning('Cannot plot %s: time or key = %s is not in table' % (log.get_filename(), key))
+
+    if log_scale:
+        ax.set_yscale('log')
+
+    ax.legend(names, loc='lower right')
+    out_file.savefig(fig)
+
+def print_filenames(logs):
+    print('files in this group: ')
+    for log in logs:
+        print(log.get_filename())
+
+def label_for_x(variable):
+    if variable == 'bw':
+        return 'Bandwidth'
+    elif variable == 'delay':
+        return 'Latency'
+    elif variable == 'loss':
+        return 'Loss rate'
+    elif variable == 'queue':
+        return 'Buffer size'
+    else:
+        raise ValueError()
+
+def label_for_y(y):
+    if y == 'util':
+        return 'Link Utilization'
+    elif y == 'rtt':
+        return 'Self-inflicted Latency'
+    else:
+        raise ValueError()
+
+def plot(x, y, data, outfile):
+    fig, ax = plt.subplots()
+    ax.set_xlabel('%s' % label_for_x(x))
+    ax.set_ylabel(label_for_y(y))
+    ax.set_title('%s sensitivity' % label_for_x(x))
+
+    method_data = data.groupby('method')
+    names = []
+
+    for method_name, group in method_data:
+        names.append(method_name)
+        ylim = (0, 1.2) if y == 'util' else None
+        group.sort_values(x).plot(x=x, y=y, ax=ax, marker='x', ylim=ylim)
+    ax.legend(names, loc='lower right')
+    outfile.savefig(fig)
+
+def process_setup(variable, logs, outfile):
+    # Currently kept in sync with Mininet script manually, but should be exported to filename...
+    network_bw = 30
+
+    rows = []
+    for log in logs:
+        contents = log.get_contents()
+        try:
+            tag = log.get_experiment_tag()
+            param = get_expt_param(tag)
+            throughput = contents['rate'].quantile(0.5) # mean is too sensitive to outliers
+            utilization = throughput / (int(param) if variable == 'bw' else network_bw)
+            mean_rtt = contents['rtt'].mean()
+            rows.append({'method': log.get_meta('method'), variable: int(param), 'util': utilization, 'rtt': mean_rtt})
+        except KeyError:
+            print_warning('%s is corrupted' % log.get_filename())
+    data = pd.DataFrame(rows)
+    plot(variable, 'util', data, outfile)
+    plot(variable, 'rtt', data, outfile)
+
+def process(log_groups, out_dir):
+    output_filename = os.path.join(out_dir, 'figure6.pdf')
+    print('Writing to %s' % output_filename)
+    output = pdf.PdfPages(output_filename)
+
+    for expt_typ, logs in log_groups.items():
+        process_setup(expt_typ, logs, output)
+
+    output.close()
+
+def lookup(filename, key):
+    without_ext = os.path.splitext(filename)[0]
+    for kv in without_ext.split('--'):
+        pair = kv.split(':')
+        if len(pair) == 2 and pair[0] == key:
+            return pair[1]
+    return ''
+
+def get_expt_type(tag):
+    return tag.split('-')[1]
+
+def get_expt_param(tag):
+    [_, _variable, param] = tag.split('-')
+    return param
+
+def group_files_by_type(logs):
+    d = {'bw': [], 'delay': [], 'loss': [], 'queue': []}
+    for log in logs:
+        tag = log.get_experiment_tag()
+        typ = get_expt_type(tag)
+        if typ == 'single':
+            continue # ignore single runs
+        d[typ].append(log)
+    return d
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Graph the sender's trace dumped by PCC client")
+    parser.add_argument('--title', help='title of file')
+    parser.add_argument('--dir', '-d', help='directory containing log files', required=True)
+    parser.add_argument('--out', '-o', help='directory to dump output files', default='./testing_logs')
+    args = parser.parse_args()
+    files = [os.path.join(args.dir, f) for f in os.listdir(args.dir)
+             if lookup(f,'expt').startswith('figure6-') and lookup(f,'host') == 'client']
+    print('Opening the following files: %s' % files)
+
+    figure6_data = [FileData(f) for f in files]
+    experiments = group_files_by_type(figure6_data)
+    process(experiments, args.out)
---
+++ run_figure6.py
@@ -0,0 +1,195 @@
+import command.command as command
+import argparse
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.link import TCLink, TCIntf
+
+from mininet.util import dumpNodeConnections, custom
+from mininet.log import setLogLevel
+
+from time import sleep
+import random
+
+default_bw, default_delay, default_loss, default_queue = 30, None, None, None
+default_run_length = 45 # give a longer time to stabilize
+
+class SingleLinkTopo(Topo):
+    'Single link between client and server.'
+
+    # bandwidth is in Mbits/second, delay is milliseconds by default, loss is percentage
+    def build(self, bw, delay, loss, queue):
+        client = self.addHost('client')
+        server = self.addHost('server')
+        self.addLink(client, server, cls=TCLink, bw=bw, delay=delay, loss=loss, max_queue_size=queue)
+
+class Setup:
+    def __init__(self, cmd, bw, delay, loss, queue):
+        'Set network parameter defaults (controls) for the setup'
+        self.cmd = cmd
+        self.params = dict()
+        self.params['bw'] = bw
+        self.params['delay'] = delay
+        self.params['loss'] = loss
+        self.params['queue'] = queue
+
+    def _run(self, how_long, bw=None, delay=None, loss=None, queue=None, tag=None):
+        'Run single-link network between server and client at set parameters '
+        'and write to file with tag'
+        
+        bw = self.params['bw'] if bw is None else bw
+        delay = self.params['delay'] if delay is None else delay
+        loss = self.params['loss'] if loss is None else loss
+        queue = self.params['queue'] if queue is None else queue
+
+        topo = SingleLinkTopo(bw, delay, loss, queue)
+        print('NETWORK IS SET AT (%s Mbps, %s ms, %s percent, %s packets)' % (bw, delay, loss, queue))
+        net = Mininet(topo)
+        net.start()
+        dumpNodeConnections(net.hosts)
+        net.pingAll()
+        # net.iperf()
+
+        client = net.get('client')
+        server = net.get('server')
+        print('SERVER IS RUNNING ON %s' % server.IP())
+
+        if tag is None:
+            tag = 'figure6-single--bw:%d--delay:%s--loss:%s--queue:%s' % (bw, delay, loss, queue)
+
+        # Experiments can sometimes fail, and a possible reason is that
+        # ports are not freed up fast enough.
+        port = random.randrange(9000, 9100)
+        self.cmd.set_server_port(port)
+        
+        client_cmd = self.cmd.get_mininet_client_cmd(server.IP(), expt_tag=tag)
+        server_cmd = self.cmd.get_mininet_server_cmd(expt_tag=tag)
+        
+        print('RUNNING CMD ON CLIENT: %s' % client_cmd)
+        print('RUNNING CMD ON SERVER: %s' % server_cmd)
+        
+        server.cmd(server_cmd)
+        client.cmd(client_cmd)
+        sleep(how_long)
+        net.stop()
+
+    def run_experiment(self, how_long, variable_data=None):
+        'Run a single experiment for how_long seconds at default seconds if '
+        'variable_data is not set, otherwise run a set of experiments with '
+        'variable varying over a set of values according to variable_data.'
+
+        if variable_data is None:
+            # just run at defaults
+            self._run(how_long)
+        else:
+            variable, params = variable_data
+            for param in params:
+                tag = 'figure6-%s-%d' % (variable, param)
+                if variable == 'bw':
+                    self._run(how_long, bw=param, tag=tag)
+                elif variable == 'delay':
+                    self._run(how_long, delay=param, tag=tag)
+                elif variable == 'loss':
+                    self._run(how_long, loss=param, tag=tag)
+                elif variable == 'queue':
+                    self._run(how_long, queue=param, tag=tag)
+
+def run_sweep(aurora, vivace, cubic3, variable):
+
+    # Run multiple experiments with these as controls
+    bw, delay, loss, queue = default_bw, default_delay, default_loss, default_queue
+    how_long = default_run_length
+
+    aurora = Setup(aurora_cmd, bw=bw, delay=delay, loss=loss, queue=queue)
+    vivace = Setup(vivace_cmd, bw=bw, delay=delay, loss=loss, queue=queue)
+    cubic3 = Setup(cubic_iperf3_cmd, bw=bw, delay=delay, loss=loss, queue=queue)
+
+    aurora.cmd.set_utility('vivace')
+    aurora.cmd.set_history_len(10)
+    cubic3.cmd.set_lifetime(how_long)
+    
+    if variable == 'bw':
+        intervals = [1, 8, 16, 32, 64, 96, 128]
+    elif variable == 'delay':
+        intervals = [1, 16, 32, 64, 128, 256, 384, 512]
+    elif variable == 'loss':
+        intervals = [1, 2, 3, 4, 5, 6, 7, 8]
+    elif variable == 'queue':
+        intervals = [1, 100, 500, 1000, 2500, 5000, 7500, 10000]
+    else:
+        raise ValueError()
+    
+    aurora.run_experiment(how_long, variable_data=(variable, intervals))
+    cubic3.run_experiment(how_long, variable_data=(variable, intervals))
+    vivace.run_experiment(how_long, variable_data=(variable, intervals))
+
+def run_single_test(aurora_cmd, vivace_cmd, cubic_cmd, bw, delay, loss, queue):
+    how_long = default_run_length
+
+    aurora = Setup(aurora_cmd, bw=bw, delay=delay, loss=loss, queue=queue)
+    vivace = Setup(vivace_cmd, bw=bw, delay=delay, loss=loss, queue=queue)
+    cubic3 = Setup(cubic_cmd, bw=bw, delay=delay, loss=loss, queue=queue)
+
+    aurora.cmd.set_utility('vivace')
+    aurora.cmd.set_history_len(10)
+    cubic3.cmd.set_lifetime(how_long)
+    
+    aurora.run_experiment(how_long)
+    cubic3.run_experiment(how_long)
+    vivace.run_experiment(how_long)
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run PCC tests")
+    parser.add_argument('--rl', '-rl', help='path to reinforcement-learning repo', required=True)
+    parser.add_argument('--model', '-m', help='path to model', required=True)
+    parser.add_argument('--aurora', '-au', help='path to aurora root', required=True)
+    parser.add_argument('--vivace', '-vi', help='path to vivace root', default='./')
+    parser.add_argument('--log', '-l', help='where to store log files', default='./testing_logs')
+    parser.add_argument('--all-bw', help='run bandwidth test (fig 6a)', default=False, action='store_true')
+    parser.add_argument('--all-delay', help='run latency test (fig 6b)', default=False, action='store_true')
+    parser.add_argument('--all-queue', help='run bandwidth test (fig 6c)', default=False, action='store_true')
+    parser.add_argument('--all-loss', help='run latency test (fig 6d)', default=False, action='store_true')
+    parser.add_argument('--all', help='run all tests in fig 6d', default=False, action='store_true')
+    parser.add_argument('--bw', help='run a single setup with bw', default=None)
+    parser.add_argument('--delay', help='run a single setup with delay', default=None)
+    parser.add_argument('--queue', help='run a single setup with queue', default=None)
+    parser.add_argument('--loss', help='run a single setup with loss', default=None)
+
+    args = parser.parse_args()
+
+    aurora_cmd = command.AuroraCmd('aurora', path=args.aurora, rlpath=args.rl,
+                                   model_path=args.model, log_path=args.log)
+    vivace_cmd = command.VivaceCmd('vivace', args.vivace, log_path=args.log)
+    cubic_iperf3_cmd = command.CubicIperfCmd('cubic3', version=3,
+                                             log_path=args.log, lifetime=30)
+
+    if args.all:
+        args.all_bw, args.all_delay, args.all_queue, args.all_loss = True, True, True, True
+
+    single_selected = args.bw or args.delay or args.queue or args.loss
+    all_selected = args.all_bw or args.all_delay or args.all_queue or args.all_loss
+
+    if (single_selected and all_selected) is True or (single_selected or all_selected) is False:
+        all_usage = '(--all-bw | --all-delay | --all-queue | --all-loss | --all)'
+        single_usage = '(--bw BW --delay DELAY --queue QUEUE --loss LOSS), leave blank for default'
+        print('Choose %s or %s.' % (all_usage, single_usage))
+        exit(0)
+
+    if args.all_bw is True:
+        run_sweep(aurora_cmd, vivace_cmd, cubic_iperf3_cmd, variable='bw')
+    if args.all_delay is True:
+        run_sweep(aurora_cmd, vivace_cmd, cubic_iperf3_cmd, variable='delay')
+    if args.all_queue is True:
+        run_sweep(aurora_cmd, vivace_cmd, cubic_iperf3_cmd, variable='queue')
+    if args.all_loss is True:
+        run_sweep(aurora_cmd, vivace_cmd, cubic_iperf3_cmd, variable='loss')
+
+    if single_selected:
+        bw = int(args.bw) if args.bw is not None else default_bw
+        delay = int(args.delay) if args.delay is not None else default_delay
+        loss = float(args.loss) if args.loss is not None else default_loss
+        queue = int(args.queue) if args.queue is not None else default_queue
+
+        print('Running single test: bw: %s, delay: %s, loss: %s, queue: %s' % (bw, delay, loss, queue))
+        
+        run_single_test(aurora_cmd, vivace_cmd, cubic_iperf3_cmd, bw, delay, loss, queue)
